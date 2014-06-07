e166ccfc4b560b942af6b2f5486a6725becc13f0
jeremy <jeremy.hintz@gmail.com>
2014-06-07 18:03:21 -0500

    Creating project, adding storyboard, mapviewcontroller

---
+++ GamePlanIOS/GamePlanIOS/GPMapViewController.m
@@ -0,0 +1,49 @@
+//
+//  GPMapViewController.m
+//  GamePlanIOS
+//
+//  Created by Jeremy Hintz on 6/7/14.
+//  Copyright (c) 2014 Game Plan. All rights reserved.
+//
+
+#import "GPMapViewController.h"
+
+@interface GPMapViewController ()
+
+@end
+
+@implementation GPMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
